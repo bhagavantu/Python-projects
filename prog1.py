5b0e920020e36e7528511b20f210cea0f8345995
Bhagavantu <bhagavantu1997@gmail.com>
2019-03-29 19:27:28 +0530

    add new print statement

--- prog1.py
+++ prog1.py
@@ -1,8 +1,10 @@
-number=200
+number=15
 
-if number > 50:
+if number < 50:
 	print("number is less than 50")
 elif number > 50 and number < 100:
     print("number is between 50 and 100") 
 else:
 	print(" number is greater than 100")
+
+print("hello!")
