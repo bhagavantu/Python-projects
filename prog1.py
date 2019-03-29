32e0c2c74a3ae5293ed8b61a06142f681e5d925c
Bhagavantu <bhagavantu1997@gmail.com>
2019-03-29 19:19:51 +0530

    add program 1 file

---
+++ prog1.py
@@ -0,0 +1,8 @@
+number=200
+
+if number > 50:
+	print("number is less than 50")
+elif number > 50 and number < 100:
+    print("number is between 50 and 100") 
+else:
+	print(" number is greater than 100")
